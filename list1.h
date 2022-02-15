511cd8df4c875fa089a7453a56bbbec41e2324ba
Josh Marusek <jm553020@ohio.edu>
2022-02-15 17:18:16 -0500

    download project files

---
+++ list1.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+struct Node {
+    std::string data;
+    Node *next;
+};
+
+class Lilist {
+    public:Lilist() {head = NULL;}
+    void add(std::string item);
+    void show();
+    private:Node *head;
+};
+
+void Lilist::add(std::string item) {
+    Node * tmp;
+    if(head == NULL) {
+        head = new Node;
+        head -> data = item;
+        head -> next= NULL;
+    }
+    else {
+        for(tmp = head; tmp -> next != NULL; tmp = tmp -> next)
+            ;  // this loop simply advances the pointer to last node in
+               // the list
+        tmp -> next = new Node;
+        tmp = tmp -> next;
+        tmp -> data = item;
+        tmp -> next = NULL;
+    }
+}
+
+void Lilist::show() {
+    for(Node *tmp = head; tmp != NULL; tmp = tmp -> next)
+        std::cout << tmp -> data << "  ";
+}
